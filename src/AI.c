121e1ebdbe61cc3d19b0e80f3f064d9468dd3a92
esion22 <loisuperloi22@gmail.com>
2024-07-16 19:13:32 +0200

    first AI using only minimax algorithm

--- src/AI.c
+++ src/AI.c
@@ -319,7 +319,6 @@ float evaluation (int* state, int depth) {
         }
     }
     float evaluation = wins*10 - losses*20 + AI_three_streaks*3 - human_three_streaks*3 - human_two_streaks*2 + AI_block_streak * 3;
-    //printf("evaluation: %d*10 - %d*10 + %d*5 - %d*5 - %d*3 + %d*7 = %f\n", wins, losses, AI_three_streaks, human_three_streaks, human_two_streaks, AI_block_streak, evaluation);
     return evaluation;
 }
 
@@ -407,85 +406,6 @@ int is_win (int* state) {
     return 0;
 }
 
-struct minMaxNode* generateChild(struct minMaxNode** parent, int childNumber, int playing, struct minMaxNode** max, struct visitedState** closedList, int* total) {
-    struct minMaxNode* child = malloc(sizeof(struct minMaxNode));
-    int i = 0;
-    int* a;
-    int invertPlaying;
-    struct visitedState* curr;
-    struct minMaxNode* maxChildEv = NULL;
-    struct minMaxNode* minChildEv = NULL;
-    int is_equal = 0;
-
-    child->state = result((*parent)->state, childNumber, playing);
-    if (child->state == NULL) {
-        free(child);
-        return NULL;
-    }
-    curr = (*closedList);
-    if ((*closedList)->next == NULL) {
-        is_equal = 1;
-    }
-    while (curr->next!=NULL) {
-        for (i = 0; i<42; i++) {
-            if (child->state[i] != curr->state[i]) {
-                is_equal = 1;
-                break;
-            }
-        }
-        curr = curr->next;
-    }
-    if (is_equal == 0) {
-        //printf("FREEEING CHILD!!!!!!!!!!!!!!!!!!!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        free(child);
-        return NULL;
-    }
-
-    struct visitedState* newNode = malloc(sizeof(struct visitedState*));
-    newNode->state = child->state;
-    newNode->next = NULL;
-    curr->next = newNode;
-    //printf("child number %d   ---   ", *total);
-    a = actions(child->state);
-    child->depth = (*parent)->depth + 1;
-    if (child->depth>=5) {
-        //printf("\n");
-        free(child);
-        return NULL;
-    }
-    child->parent = (*parent);
-    child->evaluation = evaluation(child->state, child->depth);
-    if (playing == 1) {
-        invertPlaying = 0;
-    } else {
-        invertPlaying = 1;
-    }
-    child->MaxOrMin = invertPlaying;
-    child->alpha = (*parent)->alpha;
-    child->beta = (*parent)->beta;   
-
-    if (is_win(child->state) != 0) {
-        for (i = 0; i<7; i++) {
-            child->children[i] = NULL;
-        }
-
-        return child;
-    }
-
-    int is_leaf = 0; //0 if leaf, 1 if not
-    int is_child_leaf = 0; //0 if leaf, 1 if not
-    int j = 0;
-    for (i = 0; i<7; i++) {
-        if (actions(child->state)[i]==1) {
-            *total = *total + 1;
-            child->children[i] = generateChild(&child, i, invertPlaying, max, closedList, total);
-        } else {
-            child->children[i] = NULL;
-        }
-    }
-    return child;
-}
-
 void destroyTree(struct minMaxNode** root) {
     int i = 0;
 
@@ -494,9 +414,6 @@ void destroyTree(struct minMaxNode** root) {
             destroyTree(&((*root)->children[i]));
         }
     }
-    /*if ((*root)->state!=NULL) {
-        free((*root)->state);
-    }*/
     if ((*root)!=NULL) {
         free(*root);
     }
@@ -565,139 +482,6 @@ int checkNode (struct minMaxNode* root) {
     }
 
 }
-
-/*int MinMax (int* state, int playing) {
-    //playing is 1 if the AI is playing, 0 if the human is
-    int check = 0;
-    struct minMaxNode* root = malloc(sizeof(struct minMaxNode));
-    struct minMaxNode* curr = NULL;
-    int* a = actions(state);
-    int i = 0;
-    struct minMaxNode* max = root;
-    struct visitedState* head = malloc(sizeof(struct visitedState));
-    root->state = state;
-    root->depth = 0;
-    root->evaluation = evaluation(state, 0);
-    root->parent = NULL;
-    root->MaxOrMin = 1;
-    root->alpha = INT_MIN;
-    root->beta = INT_MAX;
-    head->state = root->state;
-    head->next = NULL;
-
-    int total = 0;
-    int* p = &total;
-
-    for (i = 0; i<7; i++) {
-        if (actions(root->state)[i]) {
-            root->children[i] = generateChild(&root, i, playing, &max, &head, p);
-
-            printf("generated child #%d of root\n", i);
-            total++;
-        } else {
-            root->children[i] = NULL;
-        }
-    }
-
-    //now go from max until root. When reached, get which move he made and return that move
-    int is_leaf = 1; //0 if leaf, 1 if not
-    int j = 0;
-    struct minMaxNode* next = NULL;
-    curr = root;
-    srand(time(NULL));
-    //for every max node I need to check all its children, then I choose the min node based on the minimum evaluation. When I reach a leaf, if it is a max node I need to pick the greatest, if it's a min I need to pick the smallest
-    //root is always max node
-    int result;
-    int minEv = INT_MAX;
-    int maxEv = INT_MIN;
-    int move;
-    for (i = 0; i<7; i++) {
-        result = checkNode(curr->children[i]);
-        if (result > maxEv) {
-            maxEv = result;
-            move = i;
-        }
-    }
-    
-    /*while (is_leaf!=0) {
-        is_leaf = 0;
-        for (i = 0; i<7; i++) {
-            if (curr->children[i]!=NULL) {
-                is_leaf = 1;
-            }
-        }
-        if (is_leaf != 0) {
-            if (curr->MaxOrMin == 1) {
-                //max node
-                printf("max node\n");
-                int maxEv = INT_MIN;
-                for (i = 0; i<7; i++) {
-                    if (curr->children[i] != NULL) {
-                        printf("i: %d -> %f\n", i, curr->children[i]->evaluation);
-                        if (curr->children[i]->evaluation == maxEv) {
-                            if ((rand()%6) == 0) {
-                                printf("changing curr -> %d ev: %f\n", i, curr->children[i]->evaluation);
-                                next = curr->children[i];
-                                maxEv = curr->children[i]->evaluation;
-                            }
-                        }
-                        if (curr->children[i]->evaluation > maxEv) {
-                            printf("changing curr -> %d ev: %f\n", i, curr->children[i]->evaluation);
-                            next = curr->children[i];
-                            maxEv = curr->children[i]->evaluation;
-                        }
-                    }
-                }
-            } else {
-                //min node
-                printf("min node\n");
-                int minEv = INT_MAX;
-                for (i = 0; i<7; i++) {
-                    if (curr->children[i] != NULL) {
-                        if (curr->children[i]->evaluation == minEv) {
-                            if ((rand()%6) == 0) {
-                                printf("changing curr -> %d ev: %f\n", i, curr->children[i]->evaluation);
-                                next = curr->children[i];
-                                minEv = curr->children[i]->evaluation;
-                            }
-                        }
-                        if (curr->children[i]->evaluation < minEv) {
-                            printf("changing curr -> %d ev: %f\n", i, curr->children[i]->evaluation);
-                            next = curr->children[i];
-                            minEv = curr->children[i]->evaluation;
-                        }
-                    }
-                }
-            }
-
-            curr = next;
-            printf("curr->ev: %f\n\n", next->evaluation);
-        }
-    }*//*
-
-    printf("final ev: %d\n", maxEv);
-
-    return move;
-    
-    int count = 0;
-    struct visitedState* list = head;
-    while (curr->parent != root && curr->parent!=NULL) {
-        curr = curr->parent;
-    }
-    for (i = 0; i<7; i++) {
-        if (root->children[i] == curr) {
-            while (list->next!=NULL) {
-                count++;
-                list = list->next;
-            }
-            printf("closed list size: %d\n", count);
-            destroyList(head);
-            //destroyTree(root);
-            return i;
-        }
-    }
-    
-}*/
 
 float max (float num1, float num2) {
     if (num1>=num2) {
